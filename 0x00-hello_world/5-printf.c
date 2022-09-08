3563315cbd129dac02ff8a51555d29175a883fe1
Timixojo <Ojotimileyina994@gmail.com>
2022-09-08 13:42:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Use the function printf
+ *
+ * Description: using main fuction
+ * this program prints with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
