8d89467e95aca25b9dc4bbe9e6826759399d1e8b
Timixojo <Ojotimileyina994@gmail.com>
2022-09-09 20:27:05 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - use putchar function
+ *
+ * Description: using main function
+ * this program prints single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
