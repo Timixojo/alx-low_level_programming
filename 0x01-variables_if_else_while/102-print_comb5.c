8651d5f0f2bd8e82071090182848c2e3622681dd
Timixojo <Ojotimileyina994@gmail.com>
2022-09-10 04:45:14 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,16 +3,16 @@
  * main - use putchar function
  *
  * Description: using main function
- * this program prints all possible combinations of two two-digit numbers range from 0 to 99
+ * this program prints all possible combinations of two two-digit numbers
  * Return: 0
  */
 int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 98; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = num1 + 1; num2 < 99; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
