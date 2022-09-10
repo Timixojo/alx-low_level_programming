8c50e0f7fcb624b25fd5c949ce82a5ed85828843
Timixojo <Ojotimileyina994@gmail.com>
2022-09-09 19:18:11 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - use print function
+ *
+ * Description: using main function
+ * this program prints all single number of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf('\n');
+	return (0);
+}
