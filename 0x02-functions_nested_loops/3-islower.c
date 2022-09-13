a3a414f9fcf88b513b12d8a9f04dbdc4aab6874d
Timixojo <Ojotimileyina994@gmail.com>
2022-09-13 10:15:32 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * int _islower(int c) - use return to check function
- *
+ * int _islower - use return to check function
+ * 'c': is the int that will use for argument of the function
  * Description: using int _islower(int c) function
  * this program will check for lowercase character
  * Return: 0
