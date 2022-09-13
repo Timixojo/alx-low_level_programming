6452c4c4457411e79ad0cfe08ee8e5153dc4dc2a
Timixojo <Ojotimileyina994@gmail.com>
2022-09-13 09:20:14 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,8 @@
 /**
  * main - function to prints the alphabet, in lowercase
  *
+ * Description: using main function
+ * this program uses _putchar function to print alphabet in lowercase
  * Return: 0
  */
 
@@ -11,7 +13,7 @@ void print_alphabet(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar('c');
+		_putchar(c);
 	}
 	_putchar('\n');
 }
