7d6238f3d5115584280471d7a81c7d5f940eb289
Timixojo <Ojotimileyina994@gmail.com>
2022-09-13 10:23:14 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _islower - use return to check function
+ * _islower - use return to check function
  * @c: is the int that will use for argument of the function
  * Description: using int _islower(int c) function
  * Return: 0
