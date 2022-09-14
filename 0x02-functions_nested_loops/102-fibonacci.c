63633db34a6661b4f7786abdf96fd12cbda02105
Timixojo <Ojotimileyina994@gmail.com>
2022-09-14 06:06:36 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,13 +6,13 @@
 int main(void)
 {
 	int inc;
-	unsigned long nl = 0, n2 = 1, n3;
+	unsigned long n1 = 0, n2 = 1, n3;
 
 	for (inc = 0; inc < 50; inc++)
 	{
 		n3 = n1 + n2;
 		printf("%lu", n3);
-		ni = n2;
+		n1 = n2;
 		n2 = n3;
 
 		if (inc == 49)
