947c3362a63d9f9631ec8f606f54f78c8b34af22
Timixojo <Ojotimileyina994@gmail.com>
2022-09-14 08:36:20 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,23 +21,13 @@ void print_times_table(int n)
 
 				result = digit * mult;
 
-				if (result <= 99)
+				if (result <= 15)
 					_putchar(' ');
-
-				if (result <= 9)
-					_putchar(' ');
-				if (result >= 100)
-				{
-					_putchar((result / 100) + '0');
-					_putchar((result % 10) + '0');
-				}
-				else if (result <= 99 && result >= 10)
-				{
+				else
 					_putchar((result / 10) + '0');
-				}
 					_putchar((result % 10) + '0');
 			}
-				_putchar('\n');
+			_putchar('\n')
 		}
 	}
 }
