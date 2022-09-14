b65e67332673ca1635796f388d633ebee6ef8c1d
Timixojo <Ojotimileyina994@gmail.com>
2022-09-14 08:01:00 -0700

    100	100	100	100	100	100	100	100	100 100	100	100	100	100	100	100	100	100 100	100	100	100	100	100	100	100	100 100	100	100	100	100	100	100	100	100 100	100	100	100	100	100	100	100	100 100	100	100	100	100	100	100	100	100 100	100	100	100	100	100	100	100	100 100	100	100	100	100	100	100	100	100 100	100	100	100	100	100	100	100	100 100	100	100	100	100	100	100	100	100 100	100	100	100	100	100	100	100	100 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,11 +18,17 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
+
 				result = digit * mult;
+
 				if (result <= 99)
+				{
 					_putchar(' ');
+				}
 				if (result <= 9)
+				{
 					_putchar(' ');
+				}
 				if (result >= 100)
 				{
 					_putchar((result / 100) + '0');
@@ -31,10 +37,10 @@ void print_times_table(int n)
 				else if (result <= 99 && result >= 10)
 				{
 					_putchar((result / 10) + '0');
-				}
 					_putchar((result % 10) + '0');
+				}
+				_putchar('\n');
 			}
-					_putchar('\n');
 		}
 	}
 }
