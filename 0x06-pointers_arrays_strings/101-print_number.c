1ece1ac5779080005544723634b4b455cb4887ce
Timixojo <Ojotimileyina994@gmail.com>
2022-09-24 10:49:32 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,37 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int d = 10, n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		num *= -1;
 	}
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	n = num;
+	if (n < d)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+			if (d >= 1000000000)
+				if (d == 1000000000)
+					break;
+		}
+		if (!(d >= 1000000000) || n > 100000000)
+			if (!(d == 1000000000) || n == 123456789)
+				d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
 
-	_putchar((num % 10) + '0');
 }
