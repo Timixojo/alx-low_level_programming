af87cc478ba97f56eda530b03d7b070e8510a3e7
Timixojo <Ojotimileyina994@gmail.com>
2022-09-24 15:37:21 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,9 +2,7 @@
 
 /**
  * print_number - prints an integer
- *
  * @n: integer
- * Return: void
  */
 
 void print_number(int n)
@@ -16,7 +14,9 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
+
 	if ((num / 10) > 0)
 		print_number(num / 10);
+	
 	_putchar((num % 10) + '0');
 }
